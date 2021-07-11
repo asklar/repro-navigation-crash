ba4877a777d6707bbc85c2a2430a325a9b01cc99
Alexander Sklar <asklar@microsoft.com>
2021-07-10 23:24:01 -0700

    repro app.tsx

--- App.tsx
+++ App.tsx
@@ -1,115 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, {useCallback} from 'react';
+import {SafeAreaView, Text, View, Pressable} from 'react-native';
+import {Button} from 'react-native-xaml';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from '@react-navigation/stack';
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+type RootStackParamList = {
+  Home: undefined;
+  Add: undefined;
+};
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+const Stack = createStackNavigator<RootStackParamList>();
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+const HomeScreen: React.FC<{navigation: HomeScreenNavigationProp}> = ({
+  navigation,
+}) => {
+  return (
+    <View style={{marginTop: 96}}>
+      <Pressable
+        onPress={() => {
+          navigation.navigate('Add');
+        }}>
+        <Text>Add</Text>
+      </Pressable>
+    </View>
+  );
+};
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
+type AddScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Add'>;
 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+const AddScreen: React.FC<{navigation: AddScreenNavigationProp}> = ({
+  navigation,
+}) => {
+  const goBack = useCallback(() => {
+    navigation.goBack();
+  }, []);
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
+  return (
+    <SafeAreaView>
+      <View>
+        <Pressable onPress={goBack}>
+          <View style={{paddingVertical: 8, paddingHorizontal: 32}}>
+            <Text>Go back (Pressable)</Text>
+          </View>
+        </Pressable>
+        <Button content="Go back (XAML Button)" onClick={goBack} />
+      </View>
+    </SafeAreaView>
+  );
+};
 
- export default App;
+const App = () => {
+  return (
+    <NavigationContainer
+      theme={{
+        ...DefaultTheme,
+        colors: {...DefaultTheme.colors, background: 'transparent'},
+      }}>
+      <Stack.Navigator headerMode="none" detachInactiveScreens={true}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Add" component={AddScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
